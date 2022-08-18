5491a88ebb0535226a42d0001158fc6fa453856c
kamal khalaf <kamal.farghali@procco.com>
2022-08-18 10:19:17 +0300

    add how much True, False in array

--- easy_challenge/src/main/java/com/example/easy_challenge/easy/HowMuchTrue.java
+++ easy_challenge/src/main/java/com/example/easy_challenge/easy/HowMuchTrue.java
@@ -5,13 +5,13 @@ public class HowMuchTrue {
     public static void main(String[] args){
         boolean[] answer = new boolean[]{true, false, false, true, false, false, false, false};
         System.out.println("the true count is: " + countTrue(answer));
-        System.out.println("the fale count is: " + countFalse(answer));
+        System.out.println("the false count is: " + countFalse(answer));
     }
 
     public static int countTrue(boolean[] arr) {
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == true) count++;
+        for (boolean b : arr) {
+            if (b) count++;
         }
         return count;
     }
@@ -19,7 +19,7 @@ public class HowMuchTrue {
     public static int countFalse(boolean[] arr) {
         int count = 0;
         for (boolean i : arr) {
-            if (i == false) count++;
+            if (!i) count++;
         }
         return count;
     }
