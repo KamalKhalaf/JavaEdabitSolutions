0dc6d84d1087365d493db587c6aee31013ba908e
kamal khalaf <kamal.farghali@procco.com>
2022-08-18 10:17:03 +0300

    Initial commit

---
+++ easy_challenge/src/main/java/com/example/easy_challenge/HowMuchTrue.java
@@ -0,0 +1,26 @@
+package com.example.easy_challenge;
+
+public class HowMuchTrue {
+
+    public static void main(String[] args){
+        boolean[] answer = new boolean[]{true, false, false, true, false, false, false, false};
+        System.out.println("the true count is: " + countTrue(answer));
+        System.out.println("the fale count is: " + countFalse(answer));
+    }
+
+    public static int countTrue(boolean[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == true) count++;
+        }
+        return count;
+    }
+
+    public static int countFalse(boolean[] arr) {
+        int count = 0;
+        for (boolean i : arr) {
+            if (i == false) count++;
+        }
+        return count;
+    }
+}
